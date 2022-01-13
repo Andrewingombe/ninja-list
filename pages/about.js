52bd749c97c532b7dafa59341b90095754f84d1a
Andrew Ingombe <ingombe.ani@gmail.com>
2022-01-13 11:39:54 +0200

    Simple next.js app

---
+++ pages/about.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <div>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis,
+          nihil ab? Quae officia exercitationem consequuntur soluta ab iure.
+          Itaque eligendi mollitia dignissimos ea perferendis veritatis magnam
+          molestiae iure praesentium, officiis eum? Facilis quis nulla,
+          provident reiciendis ducimus nobis delectus laboriosam tenetur rem
+          qui? Quia, ipsam nihil adipisci similique eligendi fugiat.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis,
+          nihil ab? Quae officia exercitationem consequuntur soluta ab iure.
+          Itaque eligendi mollitia dignissimos ea perferendis veritatis magnam
+          molestiae iure praesentium, officiis eum? Facilis quis nulla,
+          provident reiciendis ducimus nobis delectus laboriosam tenetur rem
+          qui? Quia, ipsam nihil adipisci similique eligendi fugiat.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
